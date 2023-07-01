9e3ab97cea2e0ae5ab067cc4ec91258198c7e6a6
sujith8897 <sujithkumar8897@gmail.com>
2023-07-01 19:13:50 +0530

    updated

--- server.js
+++ server.js
@@ -15,36 +15,54 @@ app.post("/download", (req, res) => {
     return res.send("Please provide a YouTube URL");
   }
 
-  if (format === "mp3") {
-    const audioStream = ytdl(url, {
-      quality: "highestaudio",
-      format: "audioonly",
-    });
+  try {
+    if (format === "mp3") {
+      ytdl
+        .getBasicInfo(url)
+        .then(() => {
+          const audioStream = ytdl(url, {
+            quality: "highestaudio",
+            format: "audioonly",
+          });
 
-    res.header("Content-Disposition", 'attachment; filename="audio.mp3"');
+          res.header("Content-Disposition", 'attachment; filename="audio.mp3"');
 
-    ffmpeg()
-      .input(audioStream)
-      .format("mp3")
-      .audioCodec("libmp3lame")
-      .on("error", (err) => {
-        console.error("An error occurred: " + err.message);
-        res.sendStatus(500);
-      })
-      .on("end", () => {
-        console.log("Conversion finished");
-      })
-      .pipe(res, { end: true });
-  } else if (format === "mp4") {
-    const videoStream = ytdl(url, {
-      quality: "highest",
-    });
+          ffmpeg()
+            .input(audioStream)
+            .format("mp3")
+            .audioCodec("libmp3lame")
+            .on("error", (err) => {
+              console.error("An error occurred: " + err.message);
+              res.sendStatus(500);
+            })
+            .on("end", () => {
+              console.log("Conversion finished");
+            })
+            .pipe(res, { end: true });
+        })
+        .catch((err) => {
+          res.status(400).send({ error: "Invalid YouTube URL" });
+        });
+    } else if (format === "mp4") {
+      ytdl
+        .getBasicInfo(url)
+        .then(() => {
+          const videoStream = ytdl(url, {
+            quality: "highest",
+          });
 
-    res.header("Content-Disposition", 'attachment; filename="video.mp4"');
-
-    videoStream.pipe(res);
-  } else {
-    return res.send("Unsupported format");
+          res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+          videoStream.pipe(res);
+        })
+        .catch((err) => {
+          res.status(400).send({ error: "Invalid YouTube URL" });
+        });
+    } else {
+      return res.status(400).send({ error: "Unsupported format" });
+    }
+  } catch (err) {
+    console.log({ err });
+    res.status(500).send({ error: "Something Failed!" });
   }
 });
 
